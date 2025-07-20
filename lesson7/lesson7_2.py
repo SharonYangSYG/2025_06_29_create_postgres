5526597527efa45f3434acbf9fc9bea5c6e03c86
SharonYangSYG <tingtingdondon@gmail.com>
2025-07-20 07:14:41 +0000

    modify

---
+++ lesson7/lesson7_2.py
@@ -0,0 +1,11 @@
+print("Hello! Python!")
+print("以下是Python基本資料類型")
+
+print("整數(int):", 15)
+
+print(15)#這是整數,(int)
+print(15.0)#這是浮點數,(float)
+print("15.0")#這是字串,(str)
+print('15.0')#這是字串,(str)
+print(True)#這是布林值,(bool)
+print(False)#這是布林值,(bool)
