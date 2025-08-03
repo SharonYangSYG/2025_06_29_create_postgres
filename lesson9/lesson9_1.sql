c43721fcbc9394e02e1daf78ae5f4e75086b00e0
SharonYangSYG <tingtingdondon@gmail.com>
2025-08-03 03:06:18 +0000

    modify

--- lesson9/lesson9_1.sql
+++ lesson9/lesson9_1.sql
@@ -35,3 +35,22 @@ select "name" as 站名, count("name") as 筆數,AVG("進站人數") as "進站�
 from "每日各站進出站人數" left join "台鐵車站資訊" on "車站代碼"="stationcode"
 where 日期 between '2022-01-01'and '2022-12-31'
 group by "name"
+
+/*
+* 全省各站點2022進站總人數
+*/
+select "name" as 站名, date_part('year',"日期") as 年份, count("name") as 筆數,AVG("進站人數") as "進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "車站代碼"="stationcode"
+group by "name","年份"
+
+select "name" as 站名, date_part('year',"日期") as 年份, count("name") as 筆數,AVG("進站人數") as "進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "車站代碼"="stationcode"
+where "name" = '基隆'
+group by "name","年份";
+
+
+select "name" as 站名, date_part('year',"日期") as 年份, count("name") as 筆數,AVG("進站人數") as "進站人數"
+from "每日各站進出站人數" left join "台鐵車站資訊" on "車站代碼"="stationcode"
+where "name" = '基隆'
+group by "name","年份"
+order by "進站人數" DESC;
