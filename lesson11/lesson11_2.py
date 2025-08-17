e726fba2851c24478fbe3d894d920a4543e6c1d3
SharonYangSYG <tingtingdondon@gmail.com>
2025-08-17 05:11:30 +0000

    modify

--- lesson11/lesson11_2.py
+++ lesson11/lesson11_2.py
@@ -1,13 +1,18 @@
 import streamlit as st
-import pandas as pd
-
-st.write("Hello, *World!* :sunglasses:")
+import source
 st.sidebar.title("台鐵車站資訊")
 st.sidebar.header("2023年各站進出人數")
 st.subheader("進出站人數顯示區")
+
+@st.cache_resource
+def get_stations_names():
+    """取得車站資料"""
+    return source.get_stations_names()
+
+stations = get_stations_names()
 station = st.sidebar.selectbox(
     "請選擇車站",
-    ("台北", "台中", "高雄"),
+    stations,
 )
 
 st.write("您選擇的車站:", station)
